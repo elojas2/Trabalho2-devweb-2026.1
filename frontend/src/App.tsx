9e6c47e7ccfb4ab799b54b2b54c5bfc794b9bf58
Eloyse Fernanda <eloyse.fernanda@g.globo>
2026-06-06 18:43:48 -0300

    feat(frontend): add Navbar and protectedRoute

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,22 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import ProtectedRoute from './components/ProtectedRoute'
 import LoginPage from './pages/LoginPage'
 import CadastroPage from './pages/CadastroPage'
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/livros" replace />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/cadastro" element={<CadastroPage />} />
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Navigate to="/livros" replace />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/cadastro" element={<CadastroPage />} />
+        <Route path="/livros" element={<ProtectedRoute><div>Livros (em breve)</div></ProtectedRoute>} />
+        <Route path="/meus-emprestimos" element={<ProtectedRoute><div>Empréstimos (em breve)</div></ProtectedRoute>} />
+        <Route path="/livros/novo" element={<ProtectedRoute adminOnly><div>Cadastrar livro (em breve)</div></ProtectedRoute>} />
+        <Route path="/livros/editar/:id" element={<ProtectedRoute adminOnly><div>Editar livro (em breve)</div></ProtectedRoute>} />
+      </Routes>
+    </>
   )
 }
