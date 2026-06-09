3347cd21203572229329c4e2f841dd5a470c8e53
Patricia Carvalho <patriciacarvalho@id.uff.br>
2026-06-08 22:40:27 -0300

    Lista livros emprestados, realiza devolução

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import ProtectedRoute from './components/ProtectedRoute'
 import LoginPage from './pages/LoginPage'
 import LivrosPage from './pages/LivrosPage'
+import EmprestimosPage from './pages/EmprestimosPage'
 import CadastroPage from './pages/CadastroPage'
 import LivroFormPage from './pages/LivroFormPage'
 
@@ -15,7 +16,7 @@ export default function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/cadastro" element={<CadastroPage />} />
         <Route path="/livros" element={<ProtectedRoute><LivrosPage /></ProtectedRoute>} />
-        <Route path="/meus-emprestimos" element={<ProtectedRoute><div>Empréstimos (em breve)</div></ProtectedRoute>} />
+        <Route path="/meus-emprestimos" element={<ProtectedRoute><EmprestimosPage /></ProtectedRoute>} />
         <Route path="/livros/novo" element={<ProtectedRoute adminOnly><LivroFormPage /></ProtectedRoute>} />
         <Route path="/livros/editar/:id" element={<ProtectedRoute adminOnly><LivroFormPage /></ProtectedRoute>} />
       </Routes>
